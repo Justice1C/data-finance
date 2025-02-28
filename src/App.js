c0686d16f677a584b4c8faf68a1ccc3c5fef700a
unknown <justicec566@gmail.com>
2025-02-28 13:52:07 -0800

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar.jsx';
-
+import Hero from './components/Hero.jsx';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,7 @@ const handleNav = () => {
   setNav(!nav)
 }
 
+
   return (
     <div className='flex justify-between item-center h-24 max-w-[1240px] mx-auto text-white px-4 mt-5'>
       <h1 className='w-full text-3xl font-bold text-[#00df9a]'>React.</h1>
@@ -21,7 +22,6 @@ const handleNav = () => {
       <div onClick={handleNav} className=' block md:hidden'>
         {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       
-      </div>
       <div className={!nav ?'fixed top-0 left-0 w-[60%] h-full border-r border-r-black bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
       <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>React.</h1>
       <ul className='uppercase'> 
@@ -31,6 +31,7 @@ const handleNav = () => {
         <li className='p-4 border-b border-gray-600'>About</li>
         <li className='p-4 border-b border-gray-600'>Contact</li>
       </ul>
+      </div>
       </div>
  
     </div>
