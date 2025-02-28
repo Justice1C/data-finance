7bb0892cbb729f5e4131e840ae5ed18460e7351c
unknown <justicec566@gmail.com>
2025-02-28 02:59:30 -0800

    created the Hero file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,16 +9,16 @@ const handleNav = () => {
 }
 
   return (
-    <div className='flex justify-between item-center h-24 max-w-[1240px] mx-auto text-white px-4'>
+    <div className='flex justify-between item-center h-24 max-w-[1240px] mx-auto text-white px-4 mt-5'>
       <h1 className='w-full text-3xl font-bold text-[#00df9a]'>React.</h1>
-      <ul className='flex hidden'> 
+      <ul className='hidden md:flex'> 
         <li className='p-4'>Home</li>
         <li className='p-4'>Company</li>
         <li className='p-4'>Resource</li>
         <li className='p-4'>About</li>
         <li className='p-4'>Contact</li>
       </ul>
-      <div onClick={handleNav}>
+      <div onClick={handleNav} className=' block md:hidden'>
         {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       
       </div>
