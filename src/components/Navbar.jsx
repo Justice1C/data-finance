ab482b6a86ff9bb51e8aba881586c57d3d9c24c4
Justice1C <justicec566@gmail.com>
2025-03-01 23:44:09 +0100

    changed the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const handleNav = () => {
         <li className='p-4'>Contact</li>
       </ul>
       <div onClick={handleNav} className=' block md:hidden'>
-        {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       
       <div className={nav ?'fixed top-0 left-0 w-[60%] h-full border-r border-r-black bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
       <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>React.</h1>
